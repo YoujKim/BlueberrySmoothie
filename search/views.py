364f55846d7fd3f7aba59d20cf869c489510b011
Yujeong Kim <kimyou712@naver.com>
2022-10-03 18:50:15 +0900

    fix errors

--- search/views.py
+++ search/views.py
@@ -8,11 +8,12 @@ def home(request):
 
     try:
         ticker = request.GET['ticker']
-        search_api = requests.get("https://openapi.naver.com/v1/search/blog/"+ ticker +".json)  #""안에 jason형식으로 출력되는 api 작성 일단 네이버로 넣어놓음
+        #""안에 jason형식으로 출력되는 api 작성 일단 네이버로 넣어놓음
+        search_api = requests.get("https://openapi.naver.com/v1/search/blog/"+ ticker +".json")  
         search = json.loads(search_api.content)
     except Exception as e:
         search = ""
 
-    content = ('search':search)
+    content = {'search':search}
 
     return render(request, 'search/home.html', content)
